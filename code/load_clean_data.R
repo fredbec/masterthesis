65bb7c2d8da296bfb37c3d225cdb5090b11bc8ef
Rike <f-becker@posteo.de>
2022-05-18 14:12:10 +0200

    more `here` changes

--- code/load_clean_data.R
+++ code/load_clean_data.R
@@ -6,14 +6,14 @@ here::i_am("code/load_clean_data.R")
 
 hub_data <- rbindlist(
   list(
-    fread("hub_data/data/full-data-european-forecast-hub-1.csv"),
-    fread("hub_data/data/full-data-european-forecast-hub-2.csv"),
-    fread("hub_data/data/full-data-european-forecast-hub-3.csv")
+    fread(here("hub_data", "data", "full-data-european-forecast-hub-1.csv")),
+    fread(here("hub_data", "data", "full-data-european-forecast-hub-2.csv")),
+    fread(here("hub_data", "data", "full-data-european-forecast-hub-3.csv"))
   )
 )
 
 #load model_types and rename column for merging
-model_types <- read.csv("scraper/metadata_extended.csv") |>
+model_types <- read.csv(here("scraper", "metadata_extended.csv")) |>
   select(model_abbr, model_type)|>
   rename(model = model_abbr) |>
   mutate(model = gsub("\n| ","", model))
