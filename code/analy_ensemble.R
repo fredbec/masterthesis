f584e13087a498d35b29a9ee50adc13666a5d143
Rike <f-becker@posteo.de>
2022-05-25 17:48:25 +0200

    calculate and plot overall mean&median scores

--- code/analy_ensemble.R
+++ code/analy_ensemble.R
@@ -1,10 +1,14 @@
 library(scoringutils)
 library(here)
+library(ggplot2)
+library(gridExtra)
 
 source(here("code", "load_clean_data.R"))
 source(here("code", "fun_make_ensemble.R"))
 
 here::i_am("code/analy_ensemble.R")
+
+locs <- scan(here("specs", "locs.txt"), character(), sep = ",")
 
 cvg_threshold <- scan(here("specs", "cvg_threshold.txt"), numeric())
 
@@ -13,8 +17,40 @@ cvg_threshold <- scan(here("specs", "cvg_threshold.txt"), numeric())
 hub_data <- hub_data |>
   make_ensemble(summary_function = mean) |>
   make_ensemble(summary_function = median, 
-                extra_excl = c("mean_ensemble")) 
+                extra_excl = c("mean_ensemble")) |>
+  filter(model %in% c("mean_ensemble", "median_ensemble"))
 
 
-score(hub_data)
+plotscores <- function(hub_data, loc){
+  
+  if(length(loc) > 1){
+    p_title <- "all"
+  } else {
+    p_title <- loc
+  }
+  
+  p <- hub_data |>
+    filter(location %in% loc) |>
+    score() |>
+    summarise_scores(by = c("target_type", "model")) |>
+    summarise_scores(fun = signif, digits = 2) |>
+    plot_score_table(y = "model", by = "target_type") + 
+    facet_wrap(~ target_type, nrow = 2, ncol = 1) +
+    labs(title = p_title) +
+    scale_x_discrete(
+      labels=c("IS", "disp","u-pred", "o-pred",
+               "cvg_dev", "bias", "ae")) +
+    scale_y_discrete(
+      labels = c("median", "mean")
+    )
+  
+  return(p)
+}
 
+loclist <- c(list(locs), lapply(locs, function(x) c(x)))
+plots <- lapply(loclist, function(x) plotscores(hub_data, x))
+ggsave(
+  filename = here("plots", "score_mean_med.pdf"), 
+  plot = marrangeGrob(plots, nrow=3, ncol=2), 
+  width = 12, height = 9
+)
