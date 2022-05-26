e050d7c14e5331e1427dbacf3273bd597c330498
Rike <f-becker@posteo.de>
2022-05-26 17:23:45 +0200

    actual functionality

--- code/fun_leaveout_ensemble.R
+++ code/fun_leaveout_ensemble.R
@@ -1,3 +1,5 @@
+source(here("code", "fun_make_ensemble.R"))
+
 #' Assesses ensemble performance by iteratively sampling given numbers of 
 #' model for the (mean/median) ensemble 
 #' 
@@ -8,21 +10,26 @@
 #' @param samples how many random samples of models to draw 
 
 leaveout_ensemble <- function(data,
-                              nmods = c(5, 10, 15),
+                              nmods = c(5, 7, 10),
                               samples = 10,
                               seed = 32,
                               excl = c("EuroCOVIDhub-baseline",
-                                       "EuroCOVIDhub-ensemble")){
+                                       "EuroCOVIDhub-ensemble"),
+                              cvg_threshold = NULL){
   
   set.seed(seed)
   
-  #gets models that are not in excl
-  getmodels <- function(data, loc){
+  if(!is.null(cvg_threshold)){
+    stop("flexible definition of coverage threshold is not implemented (yet)")
+  }
+  
+  #to obtain models that are above cvg threshold and not in excl
+  getmodels <- function(data){
+    #print(data)
     models <- data |>
-      filter(location == loc) |>
+      filter(cvg_incl == 1) |>
       (\(x) unique(x$model))() |>
       (\(x) x[-which(x %in% excl)])()
-
     
     #check if there are still ensembles
     is_ensemble <- grepl(".*ensemble.*", models)
@@ -35,18 +42,65 @@ leaveout_ensemble <- function(data,
     return(models)
   }
   
-  locs <- unique(data$location)
-
-  results <- NA
-  for (loc in locs){
-    for (i in 1:samples){
-      
-      models <- getmodels(hub_data, "DE") |>
-        sample(5)
-      print(models)
-      
+  #make model list per country
+  per_loc <- split(data, f = data$location)
+  models_per_loc <- lapply(per_loc, FUN = getmodels)
+    
+  
+  #locs <- unique(data$location)
+  locs_list <- c(list(locs), lapply(locs, function(x) c(x)))
+  
+  #############***#########
+  #for now: only individual locations:
+  locs_list <- lapply(locs, function(x) c(x))
+  
+  score_tabs <- list()
+  result_table <- NULL
+  
+  for (loc in locs_list){
+    for (nmod in nmods){
+      for (i in 1:samples){
+        
+        print(loc)
+        models <- sample(models_per_loc[[loc]], 5)
+        
+        #entry for location column
+        loc_col <- ifelse(length(loc)>1, "all", loc)
+        
+        #make ensembles with current set of models
+        ensembles <- data |>
+          filter(location %in% loc,
+                 model %in% models) |>
+          make_ensemble(summary_function = mean) |>
+          make_ensemble(summary_function = median,
+                        extra_excl = c("mean_ensemble")) |>
+          filter(model %in% c("median_ensemble",
+                              "mean_ensemble")) |>
+          mutate(location = loc_col,
+                 nmod = nmod)
+        
+        
+        #score resulting ensembles
+        score_tabs[[i]] <- ensembles |>
+          score() |>
+          summarise_scores(by = c("model", "target_type", 
+                                  "location", "nmod")) 
+        
+        #print(score_tabs)
+        
+      }
+      #average over all tables for set of nmod + loc 
+      result_table <- rbind(result_table,
+                            rbindlist(score_tabs)[,lapply(.SD,mean), 
+                                                  list(model, target_type, 
+                                                       location, nmod)]
+      )
     }
+    
+    #print(result_table)
   }
   
-  return("done")
+  return(result_table)
+  
+ 
 }
