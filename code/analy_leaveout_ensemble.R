21da529d10e79997dddf012ac412e66d505fe076
Rike <f-becker@posteo.de>
2022-05-27 16:43:09 +0200

    add plotting functionality

--- code/analy_leaveout_ensemble.R
+++ code/analy_leaveout_ensemble.R
@@ -2,6 +2,11 @@ library(scoringutils)
 library(here)
 
 source(here("code", "load_clean_data.R"))
-source(here("code", "fun_ensemble.R"))
+source(here("code", "fun_leaveout_ensemble.R"))
 
 here::i_am("code/analy_leaveout_ensemble.R")
+
+
+levaour <- leaveout_ensemble(hub_data, samples = 5)
+
+saveRDS(levaour, here("results", "leaveout_ensemble.RDS"))
--- code/fun_leaveout_ensemble.R
+++ code/fun_leaveout_ensemble.R
@@ -10,8 +10,8 @@ source(here("code", "fun_make_ensemble.R"))
 #' @param samples how many random samples of models to draw 
 
 leaveout_ensemble <- function(data,
-                              nmods = c(5, 7, 10),
-                              samples = 10,
+                              nmods = c(5, 7, 10, 100),
+                              samples = 100,
                               seed = 32,
                               excl = c("EuroCOVIDhub-baseline",
                                        "EuroCOVIDhub-ensemble"),
@@ -61,7 +61,12 @@ leaveout_ensemble <- function(data,
     for (nmod in nmods){
       for (i in 1:samples){
         
-        print(loc)
+        if (nmod == 100){
+          models <- models_per_loc[[loc]]
+          nmod <- "all"
+        }
+        
+        #print(loc)
         models <- sample(models_per_loc[[loc]], 5)
         
         #entry for location column
@@ -101,6 +106,69 @@ leaveout_ensemble <- function(data,
   }
   
   return(result_table)
-  
  
+}
+
+#' Assesses ensemble performance by iteratively sampling given numbers of 
+#' model for the (mean/median) ensemble 
+#' 
+#' Uses fun_make_ensemble.R
+#'
+#' @param score_data score table output from leaveout_ensemble function
+#' @param score which score to evaluate (needs to match column names as 
+#'              induced by scoringutils' score() function)
+#'              default is interval_score
+#' @param givedata should relative score data be returned instead of plot
+#' @param title optional, alternative title
+#' @param saveplot should plot be saved in pdf format?
+#' @param path where to save plot to 
+
+plot_leaveout <- function(score_data, score = "interval_score", 
+                          givedata = FALSE, title = NULL, 
+                          saveplot = TRUE,
+                          path = here("plots", "leaveout_ensemble.pdf")
+                          ){
+  
+  #divide all values by those of ensemble using all models
+  rel_data <- score_data |>
+    filter(nmod == "all") |>
+    select(model, target_type, 
+           location, interval_score) |> #dataset of only "all", to merge 
+    rename(ref = all_of(score)) |>
+    merge(score_data, 
+          by = c("model", "target_type", "location")) |>
+    mutate(relative_score = get(score)/ref) |> 
+    select(model, target_type, location, nmod,
+           relative_score, all_of(c(score)))
+  
+  
+  if(givedata){
+    return(rel_data)
+  }
+  
+  
+  if(is.null(title)){
+    title <- "Relative score of ensemble using subset of models"
+  }
+  
+  plot <- rel_data |>
+    filter(nmod != "all") |> #all obs are one
+    mutate(nmod = as.numeric(nmod)) |>
+    ggplot(aes(x = nmod, y = relative_score,
+               color = model, linetype = target_type)) +
+    geom_path() +
+    facet_wrap(~ location) + 
+    labs(x = "Number of models sampled for ensemble",
+         y = "Score relative to full ensemble",
+         title = title)
+    
+  print(plot)
+    
+  if(saveplot){
+    ggsave(
+      filename = path,
+      width = 12, height = 9
+    )
+  }
+  
 }
