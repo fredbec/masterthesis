0bdf43fffe4f972651a0ccb6bbd4063cfe4ae01a
Rike <f-becker@posteo.de>
2022-06-04 19:04:32 +0200

    include info on chosen best models

--- code/fun_best_performers_ensemble.R
+++ code/fun_best_performers_ensemble.R
@@ -10,7 +10,6 @@ source(here("code", "fun_make_ensemble.R"))
 #' @param nmods number of models to sample
 #' @param window number of weeks to consider in establishing what 
 #'             the best models are
-#' @param samples how many random samples of models to draw 
 #' @param excl models to exclude from the ensemble
 #' @param cvg_threshold minimum coverage required for models to be 
 #'            included in the sampling process
@@ -20,18 +19,11 @@ best_performers_ensemble <- function(data,
                                      score = "interval_score",
                                      nmod = 3,
                                      window = 4,
-                                     samples = 100,
                                      excl = c("EuroCOVIDhub-baseline",
                                               "EuroCOVIDhub-ensemble"),
-                                     cvg_threshold = NULL){
+                                     cvg_threshold = NULL,
+                                     return_model_data = FALSE){
   
-  #start <- data |>
-  #  select(forecast_date) |>
-  #  distinct() |>
-  #  mutate(forecast_date = sort(forecast_date)) |>
-  #  slice_head(n = 1) |>
-  #  pull(forecast_date) |>
-  #  as.Date()
   
   #filter out models that are in excl
   data <- data |>
@@ -42,11 +34,19 @@ best_performers_ensemble <- function(data,
     as.Date() |>
     sort()
   
-  bestperf_ens <- NULL
   
   stop_ind <- length(dates) - window #not window-1 since we still need one obs for forecasting
+  
+  #result containers
+  bestperf_ens <- NULL
+  model_data <- matrix(ncol = length(unique(data$model)), 
+                       nrow = stop_ind)
+  colnames(model_data) <- unique(data$model)
+  rownames(model_data) <- as.character(dates[window+1:stop_ind])
+    
   for(i in 1:stop_ind){
     
+    #dates that are in current sliding window
     sub_dates <- seq.Date(from = dates[i],
                           to = dates[i] + (window - 1) * 7,
                           by = 7)
@@ -57,11 +57,23 @@ best_performers_ensemble <- function(data,
       score() |>
       summarise_scores(by = c("model", "location", "target_type")) |>
       group_by(location, target_type) |>
-      filter(interval_score %in% sort(interval_score)[1:nmod]) |>
+      filter(get(score) %in% sort(get(score))[1:nmod]) |>
       select(model, location, target_type)
     
     
-    #keep only those models which are in best_models
+    match_models <- function(data, model_matrix){
+      mod_names <- unique(data$model)
+      matches <- match(colnames(model_matrix), mod_names)
+      return(as.numeric(!is.na(matches)))
+    }
+    
+    #mod_names <- unique(best_models$model)
+    # matches <- match(colnames(model_data), mod_names)
+    model_data[i,] <- match_models(best_models, model_data)#as.numeric(!is.na(matches))
+    
+    
+    #keep only those models which are in best_models and make 
+    #next week's prediction out of these models
     ens_preds <- data |>
       filter(forecast_date == (sub_dates[window] + 7)) |>
       merge(best_models, by = c("model", "location", 
@@ -73,26 +85,13 @@ best_performers_ensemble <- function(data,
                           "median_ensemble"))
 
     bestperf_ens <- rbind(bestperf_ens, ens_preds)
-    
-    #score_tabs[[i]] <- bestperf_ens |>
-    #  score() |>
-    #  summarise_scores(by = c("model", "location", "target_type")) |>
-    #  mutate(nmod = nmod,
-    #         window = window,
-    #         forecast_date = ) 
-    
-    #return(score_tabs)
-    
-    
 
   }
-  
-  #end <- start + ((window - 1) * 7)
-  
-  
-  
-  #dates <- seq.Date(start, end, by = 7)
 
-  return(bestperf_ens)
+  if(return_model_data){
+    return(list(bestperf_ens, model_data))
+  } else {
+    return(bestperf_ens)
+  }
   
 }
