f3a259d9c0610dc2f918d5da1c33c4e96d2e4533
Rike <f-becker@posteo.de>
2022-06-25 22:13:06 +0100

    produce results model_random_kickout

--- code/pr_model_similarity_kickout.R
+++ code/pr_model_similarity_kickout.R
@@ -9,6 +9,9 @@ source(here("code", "load_clean_data.R"))
 avail_threshold <- specs$model_similarity_kickout_avail_threshold
 avail_overlap_threshold <- specs$model_similarity_kickout_avail_overlap_threshold
 dist_fun <- specs$model_similarity_kickout_dist_fun
+nmods <- specs$model_similarity_kickout_nmods
+samples <- specs$model_similarity_kickout_number_random_samples
+seed <- specs$model_similarity_kickout_random_seed
 
 model_dists <- model_dist(hub_data,
                           avail_threshold = avail_threshold,
@@ -34,3 +37,11 @@ kickout_results <- model_similarity_kickout(hub_data,
                                             model_dists = model_dists)
 
 saveRDS(kickout_results, here("results", "model_similarity_kickout_results.RDS"))
+
+random_kickout_results <- kickout_ensemble(hub_data,
+                                           avail_threshold = avail_threshold,
+                                           nmods = nmods,
+                                           samples = samples,
+                                           seed = seed)
+
+saveRDS(random_kickout_results, here("results", "model_similarity_random_kickout_results.RDS"))
--- specs/specs.R
+++ specs/specs.R
@@ -10,12 +10,22 @@ locs <- c("DE", "PL", "GB", "FR", "CZ")
 model_similarity_kickout_avail_threshold <- 0.5
 model_similarity_kickout_avail_overlap_threshold <- 0.35
 model_similarity_kickout_dist_fun <- cramers_dist
+model_similarity_kickout_number_random_samples <- 50
+model_similarity_kickout_nmods <- c(1,2,3)
+model_similarity_kickout_random_seed <- 32
+
 
 specs <- list(dates = dates,
               avail_threshold = avail_threshold,
               locs = locs,
               model_similarity_kickout_avail_threshold = model_similarity_kickout_avail_threshold,
               model_similarity_kickout_avail_overlap_threshold = model_similarity_kickout_avail_overlap_threshold,
-              model_similarity_kickout_dist_fun = model_similarity_kickout_dist_fun)
+              model_similarity_kickout_dist_fun = model_similarity_kickout_dist_fun, 
+              model_similarity_kickout_number_random_samples = model_similarity_kickout_number_random_samples,
+              model_similarity_kickout_nmods = model_similarity_kickout_nmods,
+              model_similarity_kickout_random_seed = model_similarity_kickout_random_seed)
 
-rm(dates, avail_threshold, locs, model_similarity_kickout_avail_threshold, model_similarity_kickout_avail_overlap_threshold, model_similarity_kickout_dist_fun)
+rm(dates, avail_threshold, locs, model_similarity_kickout_avail_threshold, 
+   model_similarity_kickout_avail_overlap_threshold, 
+   model_similarity_kickout_dist_fun, model_similarity_kickout_number_random_samples,
+   model_similarity_kickout_nmods, model_similarity_kickout_random_seed)
