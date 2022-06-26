932c98a121d4a7d1d4ef1eaf889aa08a28cd3340
Rike <f-becker@posteo.de>
2022-06-26 21:47:57 +0100

    plot model_dists

--- R/plots.R
+++ R/plots.R
@@ -67,3 +67,50 @@ plot_leaveout <- function(score_data,
   }
   
 }
+
+
+#' @title COVID-19 Forecast Hub ensemble and model structure analysis
+#' 
+#' @import dplyr 
+#' @import ggplot2
+#' @description 
+#' Plots the results of leaveout_ensemble
+#'
+#' @param model_dists output from model_similarity
+#' @param saveplot should plot output be saved
+#' @param path
+#' 
+#' 
+#' @export
+#' 
+
+plot_model_similarity <- function(model_dists,
+                                  saveplot = TRUE,
+                                  path = here("plots", "model_similarity.pdf")){
+  
+  
+  dist_plot <- model_dists |>
+    data.table(keep.rownames = TRUE) |>
+    melt(id = 1) |>
+    rename(model1 = rn,
+           model2 = variable,
+           distance = value) |>
+    mutate(model1 = factor(model1, levels = order)) |>
+    ggplot(aes(x = model1, 
+               y = model2, 
+               fill = distance)) +
+    geom_tile() + 
+    theme(axis.text.x = 
+            element_text(angle = 90, vjust = 0.5, hjust=1))
+  
+  print(plot)
+  
+  if(saveplot){
+    ggplot2::ggsave(
+      filename = path,
+      width = 12, height = 9
+    )
+  }
+  
+  
+}
