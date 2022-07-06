8240500d407604aa88583fb707f3c6ccdbd39cc9
Rike <f-becker@posteo.de>
2022-07-06 14:55:20 +0100

    server test successful, prepare first main run

--- code/analy_all_combs_ensemble.R
+++ code/analy_all_combs_ensemble.R
@@ -3,19 +3,17 @@ library(masterthesis)
 
 source(here("code", "load_clean_data.R"))
 
-test_dat <- hub_data |> filter(forecast_date < as.Date("2021-06-05"),
-                              location %in% c("CZ", "DE"))
 
 #change avail
 #devtools::load_all()
 #start_time <- Sys.time()
-myres <- all_combs_ensemble(test_dat, avail_threshold = 0.5)
+myres <- all_combs_ensemble(hub_data, avail_threshold = 0.3)
 #end_time <- Sys.time()
 #end_time - start_time
 
 
-saveRDS(myres[[1]], here("results", "all_combs_ensemble_server_ens.RDS"))
-saveRDS(myres[[2]], here("results", "all_combs_ensemble_server_dists.RDS"))
+saveRDS(myres[[1]], here("results", "all_combs_ensemble_local_ens.RDS"))
+saveRDS(myres[[2]], here("results", "all_combs_ensemble_local_dists.RDS"))
 
 
 #before doing main run
