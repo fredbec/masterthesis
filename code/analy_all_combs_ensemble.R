3a151cb695365db7da48d011ce748cec14c3274d
Rike <f-becker@posteo.de>
2022-07-07 23:09:30 +0100

    remove dist computation from all_combs_ensemble

--- code/analy_all_combs_ensemble.R
+++ code/analy_all_combs_ensemble.R
@@ -3,12 +3,17 @@ library(masterthesis)
 
 source(here("code", "load_clean_data.R"))
 
-test_data <- hub_data |> filter(location == "DE", target_type == "Cases")
+test_data <- hub_data |> filter(location == "DE", 
+                                target_type == "Cases",
+                                forecast_date < "2021-05-01")
+
+devtools::load_all()
+mymoddist <- model_dist_by_fc_date(test_data, 0.3, 0.01, cramers_dist)
 
 #change avail
 #devtools::load_all()
 #start_time <- Sys.time()
-myres <- all_combs_ensemble(test_data, avail_threshold = 0.3)
+myres <- all_combs_ensemble(test_data, mymoddist, avail_threshold = 0.3)
 #end_time <- Sys.time()
 #end_time - start_time
 
