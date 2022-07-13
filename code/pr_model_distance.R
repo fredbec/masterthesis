11ab0ab48182bd7ecb2a644226ba64f331f5f258
Rike <f-becker@posteo.de>
2022-07-13 07:56:54 +0200

    leave out pr results

--- code/pr_model_distance.R
+++ code/pr_model_distance.R
@@ -6,22 +6,22 @@ source(here("code", "load_clean_data.R"))
 
 
 ###########compute pairwise model distances###############
-pairwise_model_dist <- model_dist_by_fc_date(hub_data, 0, 0, cramers_dist)
+#pairwise_model_dist <- model_dist_by_fc_date(hub_data, 0, 0, cramers_dist)
 
-pairwise_model_dist_avghor <- pairwise_model_dist |>
-  group_by(model1, model2, forecast_date, location, target_type) |>
-  summarise(avg_dist = mean(dist))
-
-
-pairwise_model_dist <- left_join(
-  pairwise_model_dist, 
-  pairwise_model_dist_avghor,
-  by = c("model1", "model2", "forecast_date", 
-         "location", "target_type")
-)
+#pairwise_model_dist_avghor <- pairwise_model_dist |>
+#  group_by(model1, model2, forecast_date, location, target_type) |>
+#  summarise(avg_dist = mean(dist))
 
 
-saveRDS(pairwise_model_dist, here("results", "pairwise_model_dists.RDS"))
+#pairwise_model_dist <- left_join(
+#  pairwise_model_dist, 
+#  pairwise_model_dist_avghor,
+#  by = c("model1", "model2", "forecast_date", 
+#         "location", "target_type")
+#)
+
+
+#saveRDS(pairwise_model_dist, here("results", "pairwise_model_dists.RDS"))
 
 
 
@@ -29,18 +29,18 @@ saveRDS(pairwise_model_dist, here("results", "pairwise_model_dists.RDS"))
 moddist <- readRDS(here("results", "pairwise_model_dists.RDS"))
 
 #nmod = 3 run
-start_time <- Sys.time()
-res_nmod3 <- all_combs_ensemble(hub_data, moddist, nmod = 3, avail_threshold = 0)
-end_time <- Sys.time()
-print("time for nmod = 3 is")
-end_time - start_time
+#start_time <- Sys.time()
+#res_nmod3 <- all_combs_ensemble(hub_data, moddist, nmod = 3, avail_threshold = 0)
+#end_time <- Sys.time()
+#print("time for nmod = 3 is")
+#end_time - start_time
 
 
 
-res_list3 <- split(res_nmod3, by = "location")
+#res_list3 <- split(res_nmod3, by = "location")
 #save results
-sapply(names(res_list3), function(loc) 
-  saveRDS(res_list3[[loc]], here("results", "all_combs_ensemble", paste0("nmod3_", loc, ".RDS"))))
+#sapply(names(res_list3), function(loc) 
+#  saveRDS(res_list3[[loc]], here("results", "all_combs_ensemble", paste0("nmod3_", loc, ".RDS"))))
 
 
 #nmod = 4 run
