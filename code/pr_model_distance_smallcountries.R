dded9f9bb3d96a56133a3c5e28ff372780dc8426
Rike <f-becker@posteo.de>
2022-07-18 14:59:47 +0200

    parallelize

--- code/pr_model_distance_smallcountries.R
+++ code/pr_model_distance_smallcountries.R
@@ -8,45 +8,31 @@ source(here("code", "load_clean_data.R"))
 moddist <- readRDS(here("results", "pairwise_model_dists.RDS"))
 
 #nmod = 4 run:
-locs <- c("GB", "CZ", "FR")
+locs <- as.list(c("GB", "CZ", "FR"))
 
 hub_data <- hub_data |>
   filter(location %in% locs)
 
-nmods <- c(3,4,5,6,7,8)
+nmods <- c(3,4,5,6,7)
 
 comp_times <- NULL
-
 for(nmod in nmods){
+  start_time <- Sys.time()
+  allres <- mclapply(locs, function(loc)
+    all_combs_ensemble(filter(hub_data, location == loc), moddist, nmod = nmod,
+                       avail_threshold = 0),
+    mc.cores = 3
+    )
+  end_time <- Sys.time()
+  run_time <- end_time - start_time
 
-  for (loc in locs){
-    
-    if(nmod == 8 & loc %in% c("FR", "GB")){
-      next
-    }
-    
-    subdat <- hub_data |>
-      filter(location == loc)
-    
-    start_time <- Sys.time()
-    res_loc <- all_combs_ensemble(subdat, moddist, nmod = nmod, avail_threshold = 0)
-    end_time <- Sys.time()
-    run_time <- end_time - start_time
-    
-    saveRDS(res_loc, 
-            here("results", "all_combs_ensemble", paste0("nmod", nmod, "_", loc, ".RDS")))
-    rm(res_loc)
-    
-    
-    comp_times <- rbind(comp_times,
-                        data.frame(location = loc,
-                                   nmod = nmod,
-                                   comp_time = run_time))
-    
-    saveRDS(comp_times, 
-            here("results", "all_combs_ensemble", "comp_times_smallcountries.RDS"))
+  saveRDS(allres,
+          here("results", "all_combs_ensemble", paste0("nmod", nmod, "_smallcountries.RDS")))
+  nrow_res <- nrow(allres)
+  rm(allres)
   
-  }
-  
-  
+  comp_times <- rbind(comp_times,
+                      data.frame(nrow = nrow_res, 
+                                 nmod = nmod,
+                                 comp_time = run_time))
 }
