53d8b75da6e6cba70782a4557a57e1181eacc53a
Rike <f-becker@posteo.de>
2022-07-19 16:10:08 +0200

    fix some mistakes

--- code/pr_model_dist_depl.R
+++ code/pr_model_dist_depl.R
@@ -14,6 +14,7 @@ nmod <- 5
 window <- 5
 no_weeks <- 3
 no_mc.cores <- 4
+avail_threshold <- 0
 
 
 #fcdates <- sort(unique(hub_data$forecast_date))[(window+1):length(unique(hub_data$forecast_date))]
@@ -21,6 +22,11 @@ no_mc.cores <- 4
 fcdates <- sort(unique(hub_data$forecast_date))[(window+1):26]
 
 
+#small helper function to get list of dates
+#this splits up fcdates into a list with sets of `no_weeks` dates
+#and attaches the previous `window` week dates (this is just
+#because the way the `all_combs_ensemble`-function works,
+#it always needs the data to have the init weeks as well)
 make_date_list <- function(fcdates, no_weeks, window){
   fcdates <- split(sort(fcdates), 
                  ceiling(seq_along(fcdates)/no_weeks))
@@ -28,43 +34,47 @@ make_date_list <- function(fcdates, no_weeks, window){
   appendates <- function(fcdate){
     mindate <- as.Date(sort(fcdate)[1])
     p_date <- mindate - window * 7
-    
-    
     rec_dates <- seq.Date(p_date,
                           mindate-7, by = 7)
-    
     return(sort(c(rec_dates, fcdate)))
   }
   
   sections <- lapply(fcdates, function(fcdate) appendates(fcdate))
   
   return(sections)
-  
 }
-
-
-
 fc_dates_list <- make_date_list(fcdates, no_weeks, window)
 
 
+#number of iterations needed to go over list
 num_its <- ceiling(length(fc_dates_list)/no_mc.cores)
+
+#init result container
 comp_times <- NULL
 
+
+#loop over locations
 for (loc in locs){
-  for(i in 1:length(fc_dates_list)){
+  print(loc)
+  for(i in 1:num_its){
     
+    #get `no_mc.cores` elements from list
     lwr <- (no_mc.cores * (i-1)) + 1
-    upr <- no_mc.cores * i
+    upr <- ifelse(no_mc.cores * i > length(fc_dates_list),
+                  length(fc_dates_list), 
+                  no_mc.cores * i)
     curr_fc_dates_list <- fc_dates_list[lwr:upr]
-    
+
+    #make results, with mclapply
     start_time <- Sys.time()
     allres <- mclapply(curr_fc_dates_list, function(fcdates)
-      all_combs_ensemble(filter(hub_data, forecast_date %in% fcdates), 
-                         moddist, nmod = nmod,
-                         avail_threshold = 0),
+      all_combs_ensemble(
+        filter(hub_data, location == loc, forecast_date %in% fcdates),
+        moddist, nmod = nmod,
+        avail_threshold = avail_threshold),
       mc.cores = no_mc.cores
-    )
-    names(allres) <- 1:length(fc_dates_list)
+      )
+    names(allres) <- lwr:upr
     end_time <- Sys.time()
     run_time <- end_time - start_time
     
@@ -78,6 +88,7 @@ for (loc in locs){
       unlist() |>
       mean()
     
+    #save memory
     rm(allres)
     
     comp_times <- rbind(comp_times,
@@ -88,7 +99,6 @@ for (loc in locs){
                                    set = i))
     
     saveRDS(comp_times, here("results", "all_combs_ensemble", "comp_times_plde.RDS"))
-    
   }
 }
   
