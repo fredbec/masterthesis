7314ed64ea63d66708c75a4412a76b3249041525
Rike <f-becker@posteo.de>
2022-07-19 13:38:42 +0200

    automize iteration over dates

--- code/pr_model_dist_depl.R
+++ code/pr_model_dist_depl.R
@@ -10,41 +10,85 @@ moddist <- readRDS(here("results", "pairwise_model_dists.RDS"))
 
 locs <- c("DE", "PL")
 targets <- as.list(unique(hub_data$target_type))
-
-
 nmod <- 5
+window <- 5
+no_weeks <- 3
+no_mc.cores <- 4
 
-for (loc in locs){
-  subdat <- filter(hub_data, location == loc)
+
+#fcdates <- sort(unique(hub_data$forecast_date))[(window+1):length(unique(hub_data$forecast_date))]
+
+fcdates <- sort(unique(hub_data$forecast_date))[(window+1):26]
+
+
+make_date_list <- function(fcdates, no_weeks, window){
+  fcdates <- split(sort(fcdates), 
+                 ceiling(seq_along(fcdates)/no_weeks))
   
-  start_time <- Sys.time()
-  allres <- mclapply(targets, function(targ)
-    all_combs_ensemble(filter(hub_data, target_type == targ), moddist, nmod = nmod,
-                       avail_threshold = 0),
-    mc.cores = 2
-  )
-  names(allres) <- targets
-  end_time <- Sys.time()
-  run_time <- end_time - start_time
+  appendates <- function(fcdate){
+    mindate <- as.Date(sort(fcdate)[1])
+    p_date <- mindate - window * 7
+    
+    
+    rec_dates <- seq.Date(p_date,
+                          mindate-7, by = 7)
+    
+    return(sort(c(rec_dates, fcdate)))
+  }
   
-  lapply(names(allres), function(targ)
-    saveRDS(allres[[targ]],
-            here("results", "all_combs_ensemble", paste0("nmod", nmod, "_", loc, targ, ".RDS")))
-  )
+  sections <- lapply(fcdates, function(fcdate) appendates(fcdate))
   
-  nrow_res <- lapply(allres, nrow) |>
-    unlist() |>
-    mean()
+  return(sections)
   
-  rm(allres)
-  
-  comp_times <- rbind(comp_times,
-                      data.frame(nrow = nrow_res, 
-                                 nmod = nmod,
-                                 location = loc,
-                                 comp_time = run_time))
-  
-  saveRDS(comp_times, here("results", "all_combs_ensemble", "comp_times_plde.RDS"))
 }
 
 
+
+fc_dates_list <- make_date_list(fcdates, no_weeks, window)
+
+
+num_its <- ceiling(length(fc_dates_list)/no_mc.cores)
+comp_times <- NULL
+
+for (loc in locs){
+  for(i in 1:length(fc_dates_list)){
+    
+    lwr <- (no_mc.cores * (i-1)) + 1
+    upr <- no_mc.cores * i
+    curr_fc_dates_list <- fc_dates_list[lwr:upr]
+    
+    start_time <- Sys.time()
+    allres <- mclapply(curr_fc_dates_list, function(fcdates)
+      all_combs_ensemble(filter(hub_data, forecast_date %in% fcdates), 
+                         moddist, nmod = nmod,
+                         avail_threshold = 0),
+      mc.cores = no_mc.cores
+    )
+    names(allres) <- 1:length(fc_dates_list)
+    end_time <- Sys.time()
+    run_time <- end_time - start_time
+    
+    
+    lapply(names(allres), function(num)
+      saveRDS(allres[[num]],
+              here("results", "all_combs_ensemble", paste0("nmod", nmod, "_", loc,"_set",num, ".RDS")))
+    )
+    
+    nrow_res <- lapply(allres, nrow) |>
+      unlist() |>
+      mean()
+    
+    rm(allres)
+    
+    comp_times <- rbind(comp_times,
+                        data.frame(nrow = nrow_res, 
+                                   nmod = nmod,
+                                   location = loc,
+                                   comp_time = run_time,
+                                   set = i))
+    
+    saveRDS(comp_times, here("results", "all_combs_ensemble", "comp_times_plde.RDS"))
+    
+  }
+}
+  
