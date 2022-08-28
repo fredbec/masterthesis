a2bc08f5ecacdd16b3c3547f654271b61f39c115
Rike <f-becker@posteo.de>
2022-08-28 16:54:49 +0200

    include fast_eval and adjust_for_size functions

--- R/utils.R
+++ R/utils.R
@@ -491,7 +491,25 @@ wasserstein_dist <- function(q_F, q_G, tau){
 #' @param q_G quantiles of distribution G
 #' @param tau quantile levels of F and G
 #' 
+#' A Riemann sum is used to approximate a pairwise Cramér distance.
+#' The approximation formula for "left_sided_riemann" is
+#' \deqn{ \text{CD}(F,G) \approx \left\{\sum^{2K-1}_{j=1}(\tau^F_j-\tau^G_j)^2(q_{i+1}-q_i)\right\} 
+#' }{ sum^{2K-1}_{j=1}(tau^F_j-tau^G_j)^2(q_{i+1}-q_i) }
+#' and the approximation formula for "trapezoid_riemann" is
+#' \deqn{ \text{CD}(F,G) \approx \left\{\frac{1}{(K+1)^2}\sum^{2K-1}_{i=1}\frac{(\tau^F_j-\tau^G_j)^2+(\tau^F_{j+1}-\tau^G_{j+1})^2}{2}(q_{i+1}-q_i)\right\} 
+#' }{ 1/((K+1)^2) * 
+#'    sum^{2K-1}_{i=1}\frac{(\tau^F_j-\tau^G_j)^2+(\tau^F_{j+1}-\tau^G_{j+1})^2}{2} *
+#'    (q_{i+1}-q_i)  }
+#' where \eqn{q_i} is an element in a vector of an ordered pooled quantiles
+#' of \code{q_F} and \code{q_G} and \eqn{\tau^F_j} and \eqn{\tau^G_j} are defined as
+#' the probability level of a quantile in \code{q_F} when \eqn{q_i} comes from \eqn{F} and
+#' the probability level of a quantile in \code{q_G} when \eqn{q_i} comes from \eqn{G},
+#' respectively.
+#' 
 #' @return Cramer distance ()
+#' 
+#' @references Wang Y, Stark A, Ray E, Bosse N, Reich N, Wattanachit N (2022). covidHubUtils: Utility
+#' functions for the COVID-19 forecast hub. R package version 0.1.7, https://github.com/reichlab/covidHubUtils. 
 
 #' @export       
 
@@ -713,3 +731,93 @@ add_tt_and_dates <- function(add_model_dat,
 }
 
 
+#' @title COVID-19 Forecast Hub ensemble and model structure analysis
+#' 
+#' @description Helper function to adjust weights based on size of model group
+#' 
+#' @param data European Hub data
+#' @param ajust_by model group that should be ajusted for
+#' 
+#' @return data, with adjusted weights
+adjust_for_size <- function(data,
+                            adjust_by = "model_type"){
+  
+  #calculate number of models in group
+  adjustor <- data |>
+    group_by(across(all_of(c(adjust_by, "location", 
+                             "target_type", "forecast_date")))) |>
+    select(model, target_type, location, forecast_date, model_type) |>
+    distinct() |>
+    arrange(forecast_date, location, target_type, model) |>
+    mutate(num_mods_adjust = 1/n()) |>
+    ungroup() |>
+    select(model, target_type, location, forecast_date, num_mods_adjust)
+  
+  adjusted_data <- data |>
+    left_join(adjustor, by = c("model", "target_type", "location", "forecast_date")) |>
+    #adjust weights by number of models in group
+    mutate(weights_adj = weights * num_mods_adjust) |>
+    select(-weights) |>
+    select(-num_mods_adjust) |>
+    rename(weights = weights_adj)
+  
+  return(adjusted_data)
+}
+
+
+#to evaluate methods
+fast_eval <- function(target, current, return_eval = FALSE,
+                      su_cols = specs$su_cols,
+                      strat_by = c("model", "target_type","location", 
+                                   "horizon", "forecast_date"),
+                      comp_avg_by = NULL){
+  common_base <- target |>
+    select(target_type, location, forecast_date)
+  
+  current <- current |>
+    semi_join(common_base, 
+              by = c("target_type", "location", "forecast_date"))
+  
+  eval <- make_eval(target, current, su_cols,
+                    strat_by = strat_by)
+  
+  
+  #Compute averages across dimensions as specified in comp_avg_by
+  if(!is.null(comp_avg_by)){
+    if(!is.list(comp_avg_by)){
+      comp_avg <- eval |>
+        group_by(across(all_of(comp_avg_by))) |>
+        summarise(target_val = mean(target_val),
+                  current_val = mean(current_val)) |>
+        mutate(rel_score = target_val / current_val,
+               model = unique(target$model),
+               average = "average")
+    } else {
+      comp_avg <- vector(mode = "list", length = length(comp_avg_by))
+      k <- 1
+      for(cab in comp_avg_by){
+        print(cab)
+        comp_avg[[k]] <- eval |>
+          group_by(across(all_of(comp_avg_by[[k]]))) |>
+          summarise(target_val = mean(target_val),
+                    current_val = mean(current_val)) |>
+          mutate(rel_score = target_val / current_val,
+                 model = unique(target$model),
+                 average = "average")
+        print(comp_avg[[k]])
+        k <- k + 1
+      }
+      comp_avg = rbindlist(comp_avg, fill = TRUE)
+    }
+    
+    eval <- rbind(eval, comp_avg, fill = TRUE)
+  }
+  
+  
+  myval <- mean(eval$target_va) / mean(eval$current_val)
+  print(myval)
+  
+  if(return_eval){
+    return(eval)
+  }
+}
